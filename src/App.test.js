0392b1eba81d81b912b98bdec24b1b92096175b2
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-07 12:54:01 +0900

    created App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
+/* global it, describe expect */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('App renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+  it('has <Input />', () => {
+    const wrapper = shallow(Input);
+    expect(wrapper.exists()).not.toBe(true);
+  });
 });
